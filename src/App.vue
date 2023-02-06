b1b0270092a5dc1ffbf9d4e2dead6ab7cddbe3bf
Arturo Saldivar <damonbalam@gmail.com>
2023-02-06 16:08:22 -0500

    avance

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue';
+import BlogPost from './components/BlogPost.vue';
 </script>
 
 <template>
 <div class="container">
   <h1>App</h1>
   <ButtonCounter />
+  <BlogPost class="mt-5" title="Post 1"/>
+  <BlogPost  title="Post 2"/>
+  <BlogPost  title="Post 3"/>
+  <BlogPost  title="Post 4"/>
 </div>
 </template>
 
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+defineProps(['title'])
+</script>
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
