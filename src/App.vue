663c9b71b1856ea2497a8be04d5f69e3c2b866c2
Arturo Saldivar <damonbalam@gmail.com>
2023-02-06 15:55:42 -0500

    se agrego bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
-<template>
-  <h1>App</h1>
-  <ButtonCounter />
-</template>
-
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue';
-
 </script>
+
+<template>
+<div class="container">
+  <h1>App</h1>
+  <ButtonCounter />
+</div>
+</template>
 
 <style lang="scss" scoped>
 
